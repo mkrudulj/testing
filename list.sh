8c3a742b04f78b277e04c3d47004e20ab5d6ae0c
mkrudulj <mkrudulj@gmail.com>
2018-03-18 13:31:36 +0100

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,6 +1,9 @@
 ls -la
 whoami
 sudo whoami
+sudo egrep -o '(vmx|svm)' /proc/cpuinfo
+
+
 #sudo apt-get install -y ubuntu-vm-builder
 #sudo apt-get install -y grub
 #sudo ubuntu-vm-builder kvm trusty --domain newvm --dest newvm --arch i386 --hostname kvmtrusty --mem 1024 --user misha --pass loz --ip 192.168.0.12 --mask 255.255.255.0 --net 192.168.0.0 --bcast 192.168.0.255 --gw 192.168.0.1 --dns 192.168.0.1 --components 'main,universe,restricted' --addpkg acpid --addpkg vim --addpkg openssh-server --addpkg avahi-daemon --libvirt qemu:///system 
