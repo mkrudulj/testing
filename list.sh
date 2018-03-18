f4e1049ff853736da06432346f59de9d45c75621
mkrudulj <mkrudulj@gmail.com>
2018-03-18 15:09:42 +0100

    Update list.sh

--- list.sh
+++ list.sh
@@ -8,9 +8,11 @@ lsmod | grep kvm
 modprobe kvm_intel
 
 ls -la /etc/apt/sources.list
-sudo cp sources.list  /etc/apt/sources.list
+#sudo cp sources.list  /etc/apt/sources.list
 ls -la /etc/apt/sources.list
-#sudo apt-get update
+sudo apt-get update
+sudo apt-get install systemd
+sudo systemctl enable --now libvirtd
 #sudo apt-get install -y ubuntu-vm-builder
 #sudo apt-get install -y grub
 #sudo ubuntu-vm-builder kvm trusty --domain newvm --dest newvm --arch i386 --hostname kvmtrusty --mem 1024 --user misha --pass loz --ip 192.168.0.12 --mask 255.255.255.0 --net 192.168.0.0 --bcast 192.168.0.255 --gw 192.168.0.1 --dns 192.168.0.1 --components 'main,universe,restricted' --addpkg acpid --addpkg vim --addpkg openssh-server --addpkg avahi-daemon --libvirt qemu:///system 
