0f2a1f739d4f3426a56870a28728a8306219bd3b
mkrudulj <mkrudulj@gmail.com>
2018-03-23 14:48:59 +0100

    Update DiscoursePrereq.sh

--- DiscoursePrereq.sh
+++ DiscoursePrereq.sh
@@ -1,14 +1,23 @@
 sudo wget -qO- https://get.docker.com/ | sh
 docker version
+
+#add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main"
+#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install postgresql-9.6
 psql -V
+
 sudo wget http://download.redis.io/releases/redis-4.0.8.tar.gz
 sudo tar xzf redis-4.0.8.tar.gz
 cd redis-4.0.8
+#apt-get install -y tcl
 sudo make
+#sudo ./make test
 #update redis.conf
 redis-server -v
+#cd src
+#./redis-server &
+
 #https://github.com/rbenv/rbenv#installation
 rbenv global 2.3.3
 rbenv version
